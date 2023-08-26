5047311adad18b780ea6a21bd672b707b67d0d26
Anas Ajaanan <aajaanan@lab1r3s13.42abudhabi.ae>
2023-08-26 17:36:51 +0400

    Add the builtin command (env)

---
+++ src/builtins/env.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 12:09:05 by aajaanan          #+#    #+#             */
+/*   Updated: 2023/08/26 17:36:24 by aajaanan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	env(t_env_var **env_var_list)
+{
+	t_env_var	*tmp;
+
+	tmp = *env_var_list;
+	while (tmp)
+	{
+		if (tmp->value)
+			ft_printf("%s=%s\n", tmp->key, tmp->value);
+		tmp = tmp->next;
+	}
+}
