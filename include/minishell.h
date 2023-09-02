78e746f8ec776f84606d0eef3e537fa0d75d404d
Anas Ajaanan <aajaanan@lab1r3s13.42abudhabi.ae>
2023-09-02 12:20:04 +0400

    Finish Norminette of minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,16 +6,29 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 14:49:51 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/09/02 08:51:12 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/09/02 12:18:12 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <limits.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+# include "../include/command.h"
 # include "../include/builtins.h"
 # include "../include/colors.h"
-# include "../include/command.h"
 # include "../include/env.h"
 # include "../include/execution.h"
 # include "../include/parsing.h"
@@ -23,18 +36,6 @@
 # include "../include/signals.h"
 # include "../include/utility.h"
 # include "../lib/libft/libft.h"
-# include <errno.h>
-# include <fcntl.h>
-# include <limits.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/stat.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <unistd.h>
 
 // #=========================@ process_args.c @===============================#
 void	process_args(t_cmd *cmd, t_params *params, int *exit_status);
