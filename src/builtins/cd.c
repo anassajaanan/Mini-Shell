114601e797d5b03e9c3d0b5d7553ef1f3a3a2aad
Anas Ajaanan <aajaanan@lab1r3s13.42abudhabi.ae>
2023-09-02 15:57:59 +0400

    Set OLDPWD to NULL in the strating

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: aajaanan <aajaanan@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 09:02:21 by aajaanan          #+#    #+#             */
-/*   Updated: 2023/09/02 15:17:34 by aajaanan         ###   ########.fr       */
+/*   Updated: 2023/09/02 15:51:01 by aajaanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,33 @@ void	cd(char **argv, int *exit_status, t_env_var *env_var_list)
 	{
 		change_to_home_directory(exit_status, env_var_list);
 		update_pwd(env_var_list);
+		return ;
+	}
+	if (ft_strcmp(argv[1], "-") == 0)
+	{
+		if (chdir(getenv_value("OLDPWD", env_var_list)) != 0)
+		{
+			ft_printf_fd(2, "minishell: cd: OLDPWD not set\n");
+			*exit_status = 1;
+		}
+		else
+		{
+			ft_printf("%s\n", getenv_value("OLDPWD", env_var_list));
+			update_pwd(env_var_list);
+		}
+		return ;
+	}
+	if (argv[1][0] == '~')
+	{
+		char *path = ft_strjoin(getenv_value("HOME", env_var_list), argv[1] + 1);
+		if (chdir(path) != 0)
+		{
+			ft_printf_fd(2, "minishell: cd: %s: No such file or directory\n", path);
+			*exit_status = 1;
+		}
+		else
+			update_pwd(env_var_list);
+		free(path);
 		return ;
 	}
 	if (chdir(argv[1]) != 0)
